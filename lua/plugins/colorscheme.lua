85756e93298dfd750c224e15a52b7ceec2d5aa1d
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-03-29 13:39:08 -0600

    Update; colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,48 @@
+-- return { 
+--   "EdenEast/nightfox.nvim",
+--   config = function()
+--     vim.cmd[[colorscheme nightfox]]
+--     require('nightfox').setup({
+--       options = {
+--         styles = {
+--           comments = "italic",
+--           keywords = "bold",
+--           types = "italic,bold",
+--         }
+--       }
+--     })
+--   end,
+-- } -- lazy
+
+
+-- -- palenight 
+-- return {
+--   'drewtempelmeyer/palenight.vim',
+--   config = function()
+--     vim.cmd[[colorscheme palenight]]
+--   end,
+--   
+-- }
+
+
+-- tokyonight-moon
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd[[colorscheme tokyonight-storm]]
+    vim.cmd[[colorscheme tokyonight-moon]]
   end,
   opts = {},
 } 
 
+-- onedark
+-- return {
+--
+-- }
 
--- {
+-- vim-enfocado
+-- return{
 --   "wuelnerdotexe/vim-enfocado",
 --   lazy = false,
 --   priority = 1000,
