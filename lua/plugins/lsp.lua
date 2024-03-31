d259df8886ec36bdc78c1a12e8173ebc7376107f
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-03-31 16:52:38 -0600

    Modified; lsp.lua create a array to servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,42 +3,60 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "williamboman/mason.nvim",
+    "folke/neodev.nvim",
   },
 
   config = function ()
+    local lspconfig_r = require("lspconfig")
+
+
+
     -- for python
-    require ("lspconfig").pyright.setup({
+    lspconfig_r.pyright.setup({
       on_attach = on_attach,
       capabilities = capabilities,
       filetypes = {"python"},
     })
     -- for C
-    require("lspconfig").clangd.setup({
+    lspconfig_r.clangd.setup({
       on_attach = function (client, bufnr)
         client.server_capabilites.signatureHelpProvider = false
         on_attach(client, bufnr)
       end,
       capabilities = capabilities,
     })
-    -- For js, ts
-    require("lspconfig").tsserver.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-    -- css               tailwindcss
-    require("lspconfig").tailwindcss.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-    -- for eslint
-    require("lspconfig").eslint.setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
+
+    local servers = {"tsserver", "tailwindcss", "eslint"}
+
+    for _, lsp in ipairs(servers) do
+      lspconfig_r[lsp].setup{
+        on_attach = on_attach,
+        capabilities = capabilities
+      }
+
+    end
     -- for html tags
+    -- npm i -g vscode-langservers-extracted
+    -- Add path for the result of command -v npm 
+    local capabilities_html = vim.lsp.protocol.make_client_capabilities()
+    capabilities_html.textDocument.completion.completionItem.snippetSupport = true
     require("lspconfig").html.setup({
+      cmd =  { "vscode-html-language-server", "--stdio" },
+      filetypes = {"html", },
       on_attach = on_attach,
-      capabilities = capabilities,
+      capabilities = capabilities_html,
+    })
+    
+    -- for lua
+    require("neodev").setup()
+    require("lspconfig").lua_ls.setup({
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          telemetry = { enable = false },
+          workspace = { checkThirdParty = false },
+        }
+      }
     })
   end,
 }
