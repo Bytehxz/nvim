a64e9de1dacd9c06a551a8a15b485a21d4a6d34b
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-04-04 17:42:45 -0600

    Added codesnap.lua plugin

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,18 @@
+return {
+  "mistricky/codesnap.nvim",
+	build = "make",
+	config = function()
+		require("codesnap").setup({
+			mac_window_bar = true,
+			title = "CodeSnap.nvim",
+			code_font_family = "CaskaydiaCove Nerd Font",
+			watermark_font_family = "Pacifico",
+			watermark = "CodeSnap.nvim",
+			-- bg_theme = "default",
+			bg_theme = "summer",
+			breadcrumbs_separator = "/",
+			has_breadcrumbs = false,
+			save_path = "/home/bytehxz/codeSnap/code.png",
+		})
+	end,
+}
