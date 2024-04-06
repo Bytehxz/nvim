25df007433aa2392655c98396101d4eb0b205130
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-04-06 14:48:38 -0600

    Updated; gitsings plugin

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,13 +7,15 @@ return {
     trouble = false,
     on_attach = function(bufnr)
       local gitsigns = require "gitsigns"
-      vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hu", gitsigns.undo_stage_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hj", gitsigns.next_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hk", gitsigns.prev_hunk, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>ghs", gitsigns.stage_hunk, { buffer = bufnr, desc = "Add to stage git" })
+      vim.keymap.set("n", "<leader>ghu", gitsigns.undo_stage_hunk, { buffer = bufnr, desc = "Remove hunk changes of stage" })
+      vim.keymap.set("n", "<leader>ghr", gitsigns.reset_hunk, { buffer = bufnr, desc = "Discard all changes on the file" })
+      vim.keymap.set("n", "<leader>ghp", gitsigns.preview_hunk, { buffer = bufnr, desc = "Preview changes selected" })
+      vim.keymap.set("n", "<leader>ghj", gitsigns.next_hunk, { buffer = bufnr, desc = "Jump next hunk changes" })
+      vim.keymap.set("n", "<leader>ghk", gitsigns.prev_hunk, { buffer = bufnr, desc = "Jump prev hunk changes" })
     end,
     max_file_length = 10000,
   },
 }
+--
+-- Added de new hunk/trozo de código cambiado
