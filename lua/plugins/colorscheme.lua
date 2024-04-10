268a0d34db8662ad63318d1ecafa60c5f007423a
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-04-09 18:46:05 -0600

    Modified the file illuminate.lua formatting | Added; colorscheme.lua change bg & fg color for visual mode in buffers

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,8 @@ return {
   priority = 1000,
   config = function ()
     vim.cmd.colorscheme "catppuccin-macchiato"
+    -- vim.cmd('highlight Visual guibg=#FF0000')
+    vim.cmd('highlight Visual guibg=#f5a97f guifg=#000000')
   end
 }
 
