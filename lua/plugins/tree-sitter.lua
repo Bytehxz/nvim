5f010db7f9b4a701f997528fe78acbf2c4afff2b
Bytehxz <juandaniel.caballerohernandez@gmail.com>
2024-04-16 17:06:44 -0600

    Updated tree-sittier for vimdoc color

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,47 +1,49 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  event = "VeryLazy",
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "go",
-      "lua",
-      "luadoc",
-      "python",
-      "javascript",
-      "html",
-      "typescript",
-      "tsx",
-      "css",
-    },
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { "org" },
-    },
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@conditional.outer",
-          ["ic"] = "@conditional.inner",
-          ["al"] = "@loop.outer",
-          ["il"] = "@loop.inner",
-        },
-      },
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	event = "VeryLazy",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"bash",
+			"go",
+			"lua",
+			"luadoc",
+			"python",
+			"javascript",
+			"html",
+			"typescript",
+			"tsx",
+			"css",
+			"vimdoc",
+			"vim"
+		},
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "org" },
+		},
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@conditional.outer",
+					["ic"] = "@conditional.inner",
+					["al"] = "@loop.outer",
+					["il"] = "@loop.inner",
+				},
+			},
+		},
+	},
 }
